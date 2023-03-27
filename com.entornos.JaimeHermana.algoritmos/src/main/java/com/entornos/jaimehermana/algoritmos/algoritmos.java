0625f51dd39515fb3393ae8446570e2311f97853
unknown <jhc0001@alu.medac.es>
2023-03-27 10:39:02 +0200

    Metodo Factoriales

--- com.entornos.JaimeHermana.algoritmos/src/main/java/com/entornos/jaimehermana/algoritmos/algoritmos.java
+++ com.entornos.JaimeHermana.algoritmos/src/main/java/com/entornos/jaimehermana/algoritmos/algoritmos.java
@@ -7,29 +7,43 @@ package com.entornos.jaimehermana.algoritmos;
 /**
  *
  * @author Jaime
- * 
+ *
  * Version1.0
- * 
- * Esta clase es para calcular numeros fibonacci,numeros factoriales y nimeros primos
+ *
+ * Esta clase es para calcular numeros fibonacci,numeros factoriales y nimeros
+ * primos
  */
 public abstract class algoritmos {
-    
+
     /**
      * Recibe un número entero y devuelve el número de fibonacci de dicho número
+     *
      * @param posicion
      * @return numero fibonacci de dicho numero
      */
-   
-	public static int fibonacci(int posicion) {
-		int siguiente = 1, actual = 0, temporal = 0;
-		for (int i = 1; i <= posicion; i++) {
-			// Si no quieres imprimirla, comenta la siguiente línea:
-			temporal = actual;
-			actual = siguiente;
-			siguiente = siguiente + temporal;
-		}
-		// Si no quieres imprimirla, comenta la siguiente línea:
-		return actual;
-	}
+    public static int fibonacci(int posicion) {
+        int siguiente = 1, actual = 0, temporal = 0;
+        for (int i = 1; i <= posicion; i++) {
+            // Si no quieres imprimirla, comenta la siguiente línea:
+            temporal = actual;
+            actual = siguiente;
+            siguiente = siguiente + temporal;
+        }
+        // Si no quieres imprimirla, comenta la siguiente línea:
+        return actual;
+    }
     
+    /**
+     * Recibe un número entero y devuelve el factorial de dicho número
+     * @param numero
+     * @return numero factorial de dicho numero
+     */
+    public int factorial(int numero) {
+        if (numero == 0) {
+            return 1;
+        } else {
+            return numero * factorial(numero - 1);
+        }
+    }
+
 }

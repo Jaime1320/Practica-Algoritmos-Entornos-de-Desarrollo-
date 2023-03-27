2cbb27be650017c0a60f643b734de497fcacc971
unknown <jhc0001@alu.medac.es>
2023-03-27 10:30:24 +0200

    Metodo fibonacci

--- com.entornos.JaimeHermana.algoritmos/src/main/java/com/entornos/jaimehermana/algoritmos/algoritmos.java
+++ com.entornos.JaimeHermana.algoritmos/src/main/java/com/entornos/jaimehermana/algoritmos/algoritmos.java
@@ -6,8 +6,30 @@ package com.entornos.jaimehermana.algoritmos;
 
 /**
  *
- * @author usuario
+ * @author Jaime
+ * 
+ * Version1.0
+ * 
+ * Esta clase es para calcular numeros fibonacci,numeros factoriales y nimeros primos
  */
 public abstract class algoritmos {
+    
+    /**
+     * Recibe un número entero y devuelve el número de fibonacci de dicho número
+     * @param posicion
+     * @return numero fibonacci de dicho numero
+     */
+   
+	public static int fibonacci(int posicion) {
+		int siguiente = 1, actual = 0, temporal = 0;
+		for (int i = 1; i <= posicion; i++) {
+			// Si no quieres imprimirla, comenta la siguiente línea:
+			temporal = actual;
+			actual = siguiente;
+			siguiente = siguiente + temporal;
+		}
+		// Si no quieres imprimirla, comenta la siguiente línea:
+		return actual;
+	}
     
 }
